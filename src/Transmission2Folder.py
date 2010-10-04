b917fee19b9912239f85c403edfc2a85f58dfddb
Sylvain Desbureaux <sylvain@desbureaux.fr>
2010-10-04 22:53:24 +0200

    seems to work :-D

--- src/Transmission2Folder.py
+++ src/Transmission2Folder.py
@@ -7,8 +7,9 @@
    @version 0.2
 """
 
-import os.path
+import os
 import re
+import string
 import logging
 import transmissionrpc
 import yaml
@@ -29,7 +30,7 @@ def get_finished_torrents(transmission_server):
   if torrents != {}:
     for key in torrents.keys():
       torrent = tc.info(key)[key]
-      if torrent.percentDone >= 1.0:
+      if torrent.progress == 100:
         logging.debug('Download of torrent ' + repr(torrent.name) + ' is finished')
         finished_torrents.append(torrent)
       
@@ -45,13 +46,13 @@ def get_torrents_with_ratio_sup(transmission_server, ratio):
   if torrents != {}:
     for key in torrents.keys():
       torrent = tc.info(key)[key]
-      if ((torrent.percentDone >= 1.0) and (torrent.uploadRatio >= ratio)):
+      if ((torrent.progress == 100) and (torrent.ratio >= ratio)):
         ratio_torrents.append(torrent)
       
   return ratio_torrents
 
 def is_included_series(name, series):
-  """ Return True if the ``name`` of the torrent is included in 
+  """ Return the name of the serie if the ``name`` of the torrent is included in 
   the array ``series``, with space can be [ ._-] """
   logging.debug('is_included_series')
   name = name.lower()
@@ -60,17 +61,40 @@ def is_included_series(name, series):
     logging.debug('regex to apply: ' + repr(regex) )
     if re.search(regex,name): 
       logging.debug('torrent with name ' + repr(name) + 'match the serie name ' + repr(serie))
-      return True
+      return serie
   logging.debug('torrent with name ' + repr(name) + ' doesn\'t match with any of the serie name')
   return False
     
+def search_season_and_episode(name, serie):
+  """ Return the season and the episode number of the serie found in the 
+  ``name`` of the torrent, just after the name of the ``serie`` """
+  regex_serie = re.sub(r" ",'[ ._-]',serie.lower())
+  regex_start = '[a-zA-Z/0-9.]*' + regex_serie + '[ ._-]*[Ss]*'
+  regex_end = '[0-9]+[eExX]+[0-9]+'
+  logging.debug('Regex to search name: ' + regex_start)
+  sub_name = re.sub(regex_start,'',name.lower())
+  logging.debug('  Result: ' + sub_name)
+  logging.debug('Regex to search season and episode: ' + regex_end)
+  season_and_episode = re.findall(regex_end,sub_name)[0]
+  logging.debug('Season and Episode found: ' + repr(season_and_episode))
+  result={}
+  result['season'] = re.split(r"[eExX]+",season_and_episode)[0]
+  logging.debug('Season Found: ' + repr(result['season']))
+  result['episode'] = re.split(r"[eExX]+",season_and_episode)[1]
+  logging.debug('Episode Found: ' + repr(result['episode']))
+  return result
 
-def link_files(files, serie_path):
-  """ Hard Link the relevant ``files`` to the ``serie_path`` """
+def link_files(files, folder, serie, episode, season):
+  """ Hard Link the relevant ``files`` to the ``destination_folder`` with the 
+  relevant name (composed with ``serie``, ``season`` and ``episode``)"""
   logging.debug('Entering link_files')
-  pass
+  for filee in files:
+    ext = '.' + filee.split('.')[-1]
+    destination = folder + serie.title() + ' - S' + season + 'E' + episode + ext
+    logging.debug('Linking file ' + repr(filee) + ' to ' + destination)
+#    os.link(filee, destination)
 
-def files_to_move(files, extensions):
+def files_to_move(files, extensions,src_folder):
   """ Return the file to move, i.e. the files with extension included in 
   ``extensions`` array"""
   logging.debug('Entering files_to_move')
@@ -81,9 +105,10 @@ def files_to_move(files, extensions):
     file_extension = filee.split('.')[-1]
     for extension in extensions:
       if extension == file_extension: 
-        logging.debug('file ' + repr(filee) + ' has an extension(' +  
-        repr('file_extension') + ') matching one of the extension allowed: ' + repr(extension))
-        files_to_move.append(filee) 
+        logging.debug('file ' + repr(filee) + ' has an extension(' +
+        repr('file_extension') + ') matching one of the extension allowed: ' +
+        repr(extension))
+        files_to_move.append(src_folder + filee) 
 
   logging.debug('files to move: ' + repr(files_to_move))
   return files_to_move
@@ -125,6 +150,8 @@ def verify_config(config):
     logging.error('destination folder not set, exiting')
     print 'destination folder not set, exiting'
     exit() -1
+  
+  if config['destination_folder'][-1] != '/': config['destination_folder'] += '/'
 
   return config
   
@@ -142,14 +169,21 @@ if os.path.exists(path + '/Transmission2Folder.yaml'):
     tc = transmissionrpc.Client(config['host'], port=config['port'])
   finished_torrents = get_finished_torrents(tc)
   for torrent in finished_torrents:
-    if is_included_series(torrent.name, config['series']):
-      files = files_to_move(torrent.files(), config['extensions'])
-      link_files(files)
+    serie = is_included_series(torrent.name, config['series'])
+    if serie:
+      src_folder = torrent.fields['downloadDir']
+      files = files_to_move(torrent.files(), config['extensions'],src_folder)
+      result = search_season_and_episode(torrent.name, serie)
+      episode = string.zfill(int(result['episode']),2)
+      season = string.zfill(int(result['season']),2)
+      folder_destination = config['destination_folder'] + serie.title() + '/Season ' + season +'/'
+      logging.debug('Making the directory(ies): ' + folder_destination)
+#      os.makedirs(folder_destination)
+      link_files(files,folder_destination, serie, episode, season)
   torrents_to_stop = get_torrents_with_ratio_sup(tc, config['ratio'])
   for torrent in torrents_to_stop:
     logging.info('Stopping torrent ' + repr(torrent.name))
-    tc.stop(torrent.id)
-    if is_included_series(torrent.name, config['series']): tc.remove(torrent.id) 
+#    tc.stop(torrent.id)
+#    if is_included_series(torrent.name, config['series']): tc.remove(torrent.id)
 else:
   print "configuration file (Transmission2Folder.yaml) doesn't exist, aborting..."
-    
